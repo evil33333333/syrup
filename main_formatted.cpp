21fea5fc507a0b8f7b98c8b5d853bdbdda729479
Eve <84185604+evil33333333@users.noreply.github.com>
2022-01-10 13:23:23 -0500

    Update main_formatted.cpp
    Rewritten.

--- main_formatted.cpp
+++ main_formatted.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <lib/base64.h>
 #include <windows.h>
 #include <thread>
 
-static
-const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-"abcdefghijklmnopqrstuvwxyz"
-"0123456789+/";
+static const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz" "0123456789+/";
 
 unsigned short RED_COLOR = 12;
 unsigned short GREEN_COLOR = 10;
 unsigned short CYAN_COLOR = 11;
+bool finished = false;
 
 static inline bool is_base64(unsigned char c)
 {
 	return (isalnum(c) || (c == '+') || (c == '/'));
 }
 
+/*
 std::string encode(unsigned char
-	const *bytes_to_encode, unsigned int in_len)
+	const* bytes_to_encode, unsigned int in_len)
 {
 	std::string ret;
 	int i = 0;
@@ -33,10 +31,10 @@ std::string encode(unsigned char
 		char_array_3[i++] = *(bytes_to_encode++);
 		if (i == 3)
 		{
-			char_array_4[0] = (char_array_3[0] &0xfc) >> 2;
-			char_array_4[1] = ((char_array_3[0] &0x03) << 4) + ((char_array_3[1] &0xf0) >> 4);
-			char_array_4[2] = ((char_array_3[1] &0x0f) << 2) + ((char_array_3[2] &0xc0) >> 6);
-			char_array_4[3] = char_array_3[2] &0x3f;
+			char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+			char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+			char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+			char_array_4[3] = char_array_3[2] & 0x3f;
 
 			for (i = 0;
 				(i < 4); i++)
@@ -50,21 +48,55 @@ std::string encode(unsigned char
 		for (j = i; j < 3; j++)
 			char_array_3[j] = '\0';
 
-		char_array_4[0] = (char_array_3[0] &0xfc) >> 2;
-		char_array_4[1] = ((char_array_3[0] &0x03) << 4) + ((char_array_3[1] &0xf0) >> 4);
-		char_array_4[2] = ((char_array_3[1] &0x0f) << 2) + ((char_array_3[2] &0xc0) >> 6);
-		char_array_4[3] = char_array_3[2] &0x3f;
+		char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+		char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+		char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+		char_array_4[3] = char_array_3[2] & 0x3f;
 
 		for (j = 0;
 			(j < i + 1); j++)
 			ret += base64_chars[char_array_4[j]];
 
-		while ((i++<3))
+		while ((i++ < 3))
 			ret += '=';
 
 	}
 
 	return ret;
+}
+*/
+
+static std::string encode(const std::string& in) {
+
+	std::string out;
+	int val = 0, valb = -6;
+	for (unsigned char c : in) {
+		val = (val << 8) + c;
+		valb += 8;
+		while (valb >= 0) {
+			out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[(val >> valb) & 0x3F]);
+			valb -= 6;
+		}
+	}
+	if (valb > -6) out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[((val << 8) >> (valb + 8)) & 0x3F]);
+	while (out.size() % 4) out.push_back('=');
+	return out;
+}
+
+void syrup(std::string content, std::string path, int cycles)
+{
+	std::string encoded_content = encode(content);
+	std::string all_encoded_content = "import base64\nexec(base64.b64decode('" + encoded_content + "'))";
+	for (int i = 0; i < cycles; i++)
+	{
+		std::string encoded_string = encode(all_encoded_content);
+		all_encoded_content = "import base64\nexec(base64.b64decode('" + encoded_string + "'))";
+	}
+	std::ofstream file;
+	file.open(path);
+	file << all_encoded_content << std::endl;
+	file.close();
+	finished = true;
 }
 
 int main()
@@ -72,9 +104,7 @@ int main()
 	HANDLE handle;
 	handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(handle, GREEN_COLOR);
-	std::cout << "Path: " << std::endl;
-	std::string path;
-	std::cin >> path;
+	std::cout << "Path: "; std::string path; std::cin >> path;
 	if (path.find(".py") == std::string::npos)
 	{
 		SetConsoleTextAttribute(handle, RED_COLOR);
@@ -82,7 +112,7 @@ int main()
 		return 1;
 	}
 
-	std::ifstream file;
+	std::ifstream file(path);
 	if (!file.is_open())
 	{
 		SetConsoleTextAttribute(handle, RED_COLOR);
@@ -90,7 +120,7 @@ int main()
 		return 1;
 	}
 
-	std::string content = std::string(std::istreambuf_iterator<char> (file), std::istreambuf_iterator<char> ());
+	std::string content = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
 	file.close();
 	if (content.compare("") == 0)
 	{
@@ -98,30 +128,15 @@ int main()
 		std::cout << "File empty. Cannot proceed." << std::endl;
 		return 1;
 	}
-
+	std::cout << "Cycles: "; int cycles; std::cin >> cycles;
 	SetConsoleTextAttribute(handle, GREEN_COLOR);
-	std::cout << "Obfuscating..." << std::endl;
-	syrup(content, path);
+	std::thread obfuscator(syrup, content, path, cycles);
+	while (!finished) 
+	{
+		std::cout << "\r[/] Obfuscating with " << cycles << " cycles.\r"; std::cout << "\r[-] Obfuscating with " << cycles << " cycles..\r"; std::cout << "\r[\\] Obfuscating with "<< cycles << " cycles...\r";
+	}
 	SetConsoleTextAttribute(handle, CYAN_COLOR);
 	std::cout << "Obfuscated your Python script :)" << std::endl;
 	std::this_thread::sleep_for(std::chrono::seconds(5));
 	return 0;
-}
-
-void syrup(std::string content, std::string path)
-{
-	const unsigned char *content_bytes = reinterpret_cast<const unsigned char*> (content.c_str());
-	std::string encoded_content = encode(content_bytes, sizeof(content_bytes));
-	std::string all_encoded_content = "import base64\nexec(base64.b64decode('" + encoded_content + "'))";
-	for (int i = 0; i < 199; i++)
-	{
-		const unsigned char *encoded_bytes = reinterpret_cast<const unsigned char*> (all_encoded_content.c_str());
-		std::string encoded_string = encode(encoded_bytes, sizeof(encoded_bytes));
-		all_encoded_content = "import base64\nexec(base64.decode('" + encoded_string + "'))";
-	}
-
-	std::ofstream file;
-	file.open(path);
-	file << all_encoded_content << std::endl;
-	file.close();
 }
