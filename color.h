b21ef242c6d8be6214663fd343f8343969681ba8
Eve <84185604+evil33333333@users.noreply.github.com>
2022-07-05 23:55:21 +0100

    Create color.h

---
+++ color.h
@@ -0,0 +1,17 @@
+#pragma once
+#define BLACK      0
+#define DARK_BLUE  1
+#define DARK_GREEN 2
+#define LIGHT_BLUE 3
+#define DARK_RED   4
+#define MAGENTA    5
+#define ORANGE     6
+#define LIGHT_GREY 7
+#define GREY       8
+#define BLUE       9
+#define GREEN     10
+#define CYAN      11
+#define RED       12
+#define PINK      13
+#define YELLOW    14
+#define WHITE     15
