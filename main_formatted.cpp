05474db6b081413b8450a67d02827a5ea7a7a3ac
Eve <84185604+evil33333333@users.noreply.github.com>
2022-07-05 23:54:22 +0100

    Update main_formatted.cpp

--- main_formatted.cpp
+++ main_formatted.cpp
@@ -3,155 +3,134 @@
 #include <sstream>
 #include <windows.h>
 #include <thread>
+#include <array>
+#include <chrono>
+#include "Color.h"
 
-static const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz" "0123456789+/";
-
-unsigned short RED_COLOR = 12;
-unsigned short GREEN_COLOR = 10;
-unsigned short CYAN_COLOR = 11;
-unsigned short YELLOW_COLOR = 14;
-bool finished = false;
-
-static inline bool is_base64(unsigned char c)
+WORD get_random_color()
 {
-	return (isalnum(c) || (c == '+') || (c == '/'));
+    std::array<WORD, 16> colors = { BLACK, BLUE, CYAN, DARK_BLUE, DARK_GREEN, DARK_RED, GREEN, GREY, LIGHT_BLUE, LIGHT_GREY, MAGENTA, ORANGE, PINK, RED, WHITE, YELLOW };
+    uint32_t index = std::rand() % colors.size();
+    return colors[index];
 }
 
-/*
-std::string encode(unsigned char
-	const* bytes_to_encode, unsigned int in_len)
+std::string base64_encode(const std::string& in)
 {
-	std::string ret;
-	int i = 0;
-	int j = 0;
-	unsigned char char_array_3[3];
-	unsigned char char_array_4[4];
 
-	while (in_len--)
-	{
-		char_array_3[i++] = *(bytes_to_encode++);
-		if (i == 3)
-		{
-			char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-			char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-			char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-			char_array_4[3] = char_array_3[2] & 0x3f;
-
-			for (i = 0;
-				(i < 4); i++)
-				ret += base64_chars[char_array_4[i]];
-			i = 0;
-		}
-	}
-
-	if (i)
-	{
-		for (j = i; j < 3; j++)
-			char_array_3[j] = '\0';
-
-		char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-		char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-		char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-		char_array_4[3] = char_array_3[2] & 0x3f;
-
-		for (j = 0;
-			(j < i + 1); j++)
-			ret += base64_chars[char_array_4[j]];
-
-		while ((i++ < 3))
-			ret += '=';
-
-	}
-
-	return ret;
-}
-*/
-int GetRandomColor() {
-	int random_number = std::rand() % 5;
-	if (random_number == 0) return RED_COLOR;
-	if (random_number == 1) return GREEN_COLOR;
-	if (random_number == 2) return CYAN_COLOR;
-	if (random_number == 3) return YELLOW_COLOR;
-	if (random_number == 4) GetRandomColor();
+    std::string out;
+    int val = 0, valb = -6;
+    for (unsigned char c : in)
+    {
+        val = (val << 8) + c;
+        valb += 8;
+        while (valb >= 0)
+        {
+            out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[(val >> valb) & 0x3F]);
+            valb -= 6;
+        }
+    }
+    if (valb > -6)
+    {
+        out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[((val << 8) >> (valb + 8)) & 0x3F]);
+    }
+    while (out.size() % 4)
+    {
+        out.push_back('=');
+    }
+    return out;
 }
 
-static std::string encode(const std::string& in) {
-
-	std::string out;
-	int val = 0, valb = -6;
-	for (unsigned char c : in) {
-		val = (val << 8) + c;
-		valb += 8;
-		while (valb >= 0) {
-			out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[(val >> valb) & 0x3F]);
-			valb -= 6;
-		}
-	}
-	if (valb > -6) out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[((val << 8) >> (valb + 8)) & 0x3F]);
-	while (out.size() % 4) out.push_back('=');
-	return out;
-}
-
-void syrup(std::string content, std::string path, int cycles)
+void syrup(std::string content, std::string path, int cycles, bool* finished)
 {
-	std::string encoded_content = encode(content); int cycles_finished = 0;
-	std::string all_encoded_content = "import base64\nexec(base64.b64decode('" + encoded_content + "'))";
-	for (int i = 0; i < cycles; i++)
-	{
-		std::string encoded_string = encode(all_encoded_content);
-		all_encoded_content = "import base64\nexec(base64.b64decode('" + encoded_string + "'))";
-		cycles_finished++;  std::string title = " title [Syrup Obfuscation] Cycles Finished [" + std::to_string(cycles_finished) + "/" + std::to_string(cycles) + "]";
-		system(title.c_str());
-	}
-	std::ofstream file;
-	file.open(path);
-	file << all_encoded_content << std::endl;
-	file.close();
-	finished = true;
+    std::string encoded_content = base64_encode(content);
+    int cycles_finished = 0;
+    std::string all_encoded_content = "import base64\nexec(base64.b64decode('" + encoded_content + "'))";
+    for (int i = 0; i < cycles; i++)
+    {
+        std::string encoded_string = base64_encode(all_encoded_content);
+        all_encoded_content = "import base64\nexec(base64.b64decode('" + encoded_string + "'))";
+        cycles_finished++;
+        std::string title = "[Syrup Obfuscation] Cycles Finished [" + std::to_string(cycles_finished) + "/" + std::to_string(cycles) + "]";
+        SetConsoleTitleA(title.c_str());
+    }
+
+    std::ofstream file(path);
+    file << all_encoded_content << std::endl;
+    file.close();
+    *finished = true;
 }
 
 int main()
 {
-	HANDLE handle;
-	handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(handle, CYAN_COLOR);
-	std::cout << "Welcome to Syrup Obfuscation :3" << std::endl;
-	std::this_thread::sleep_for(std::chrono::seconds(3));
-	SetConsoleTextAttribute(handle, GREEN_COLOR);
-	std::cout << "Path: "; std::string path; std::cin >> path;
-	if (path.find(".py") == std::string::npos)
-	{
-		SetConsoleTextAttribute(handle, RED_COLOR);
-		std::cout << "File given is not a python file." << std::endl;
-		return 1;
-	}
+    int cycles{};
+    bool finished = false;
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::array<std::string, 4> spinners = { "[/]", "[-]", "[\\]", "[|]" };
 
-	std::ifstream file(path);
-	if (!file.is_open())
-	{
-		SetConsoleTextAttribute(handle, RED_COLOR);
-		std::cout << "File given could not be opened." << std::endl;
-		return 1;
-	}
+    SetConsoleTextAttribute(handle, CYAN);
+    std::cout << "Welcome to Syrup Obfuscation :3" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(3));
 
-	std::string content = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
-	file.close();
-	if (content.compare("") == 0)
-	{
-		SetConsoleTextAttribute(handle, RED_COLOR);
-		std::cout << "File empty. Cannot proceed." << std::endl;
-		return 1;
-	}
-	std::cout << "Cycles: "; int cycles; std::cin >> cycles;
-	SetConsoleTextAttribute(handle, GREEN_COLOR);
-	std::thread obfuscator(syrup, content, path, cycles);
-	while (!finished) 
-	{
-		SetConsoleTextAttribute(handle, GetRandomColor());
-		std::cout << "\r[/] Obfuscating with " << cycles << " cycles.\r"; std::cout << "\r[-] Obfuscating with " << cycles << " cycles..\r"; std::cout << "\r[\\] Obfuscating with "<< cycles << " cycles...\r";
-	}
-	SetConsoleTextAttribute(handle, CYAN_COLOR);
-	std::cout << "Obfuscated your Python script :)" << std::endl;
-	std::this_thread::sleep_for(std::chrono::seconds(5));
-	return 0;
+    SetConsoleTextAttribute(handle, GREEN);
+    std::cout << "[+] Filename: ";
+    std::string path;
+    std::getline(std::cin, path);
+
+    if (path.find(".py") == std::string::npos)
+    {
+        SetConsoleTextAttribute(handle, DARK_RED);
+        std::cout << "[X] File given is not a python file." << std::endl;
+        return 1;
+    }
+
+    std::ifstream file(path);
+    if (!file.is_open())
+    {
+        SetConsoleTextAttribute(handle, DARK_RED);
+        std::cout << "[X] File given could not be opened." << std::endl;
+        return 1;
+    }
+
+    std::cout << "[!] File has been found!" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::string content = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+    file.close();
+    if (content.empty())
+    {
+        SetConsoleTextAttribute(handle, DARK_RED);
+        std::cout << "File empty. Cannot proceed." << std::endl;
+        return 1;
+    }
+
+    std::cout << "[!] Read all content from the file!" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    std::cout << "[+] Cycles: ";
+    std::cin >> cycles;
+
+
+    std::cout << "[+] New File Path: ";
+    std::string new_path;
+    std::getline(std::cin, new_path);
+
+    SetConsoleTextAttribute(handle, GREEN);
+
+    std::cout << "[!] Starting now....";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    
+    std::thread obfuscator(syrup, content, path, cycles, &finished);
+    while (!finished)
+    {
+        SetConsoleTextAttribute(handle, get_random_color());
+        for (auto const& spinner : spinners)
+        {
+            std::cout << "\r" << spinner << "Obfuscating with " << cycles << " cycles.\r";
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+    SetConsoleTextAttribute(handle, CYAN);
+    std::cout << "[!] Obfuscated your Python script :)" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    return 0;
 }
